d8de8a47df75c5dbc4e0f773179da31f1f4db10d
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-04-17 15:44:43 -0300

    .

--- resources/views/reports/receipt.blade.php
+++ resources/views/reports/receipt.blade.php
@@ -13,14 +13,14 @@
 @section('content')
     <table style="width: 100%;" class="striped">
         <tbody>
-            <tr style="font-size: 13px;">
+            <tr style="font-size: 18px;">
                 <td class=""><strong>Data do Atendimento:</strong>
                     {{ date('d/m/Y', strtotime($treatment->date)) }}</td>
             </tr>
-            <tr style="font-size: 13px;">
+            <tr style="font-size: 18px;">
                 <td class=""><strong>Conveniado:</strong> {{ $treatment->partner->name }}</td>
             </tr>
-            <tr style="font-size: 13px;">
+            <tr style="font-size: 18px;">
                 <td class=""><strong>Paciente:</strong> {{ $treatment->patient->name }}</td>
             </tr>
         </tbody>
