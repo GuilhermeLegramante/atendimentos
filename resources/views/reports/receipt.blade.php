ddbbbc2afa37ed6057d908bddd7e7592fd5cc596
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-04-17 14:35:10 -0300

    .

--- resources/views/reports/receipt.blade.php
+++ resources/views/reports/receipt.blade.php
@@ -5,7 +5,18 @@
 @endsection
 
 @section('content')
-    <h1>COMPROVANTE DE ATENDIMENTO</h1>
+    <table>
+        <tbody>
+            <tr class="bg-light" style="font-size: 13px;">
+                <td class="collumn-left"><strong>Data do Atendimento:</strong>
+                    {{ date('d/m/Y', strtotime($treatment->date)) }}</td>
+            </tr>
+            <tr class="bg-light" style="font-size: 13px;">
+                <td class="collumn-left"><strong>Conveniado:</strong> {{ $treatment->partner->name }}</td>
+                <td class="collumn-right"><strong>Paciente:</strong> {{ $treatment->patient->name }}</td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
 
 @section('footer')
