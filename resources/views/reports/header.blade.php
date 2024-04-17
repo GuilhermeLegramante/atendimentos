9d70a782d2c6105d399bb97db2a7c089422da58a
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-04-17 14:18:56 -0300

    .

--- resources/views/reports/header.blade.php
+++ resources/views/reports/header.blade.php
@@ -1,9 +1,9 @@
-<header style="position: fixed; margin-top: -20%;">
+<header style="position: fixed; margin-top: -15%;">
     <div style="height: 100px;">
         <table style="width: 100%;">
             <tbody>
                 <tr>
-                    <td style="height: 200px; vertical-align: middle; width: 15%;">
+                    <td style="height: 200px; vertical-align: middle; width: 18%;">
                         <img src="{{ public_path('img/logo.png') }}" style="width: 200px; height: 75px;">
                     </td>
                     <td style="height: 64px; width: 30%;">
