e9f5ff6aafc420c07db259ae10cd2f12bb9482c8
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-05-15 09:54:29 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('/sinais', function () {
     }
 
     foreach ($signals as $key => $signal) {
-        if ($signal['id'] > 4102) {
+        if ($signal['id'] > 4617) {
             $farmer = DB::connection('marcaesinal')->table('agro_produtor')
                 ->join('hscad_cadmunicipal', 'hscad_cadmunicipal.inscricaomunicipal', '=', 'agro_produtor.idmunicipe')
                 ->select(
