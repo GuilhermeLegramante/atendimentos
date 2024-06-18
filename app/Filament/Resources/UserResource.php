79f63ee0c3b80bc822755f6fd2853c0b3f648453
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-06-18 13:09:33 -0300

    .

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -10,6 +10,8 @@ use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\Person;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
@@ -43,49 +45,55 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')
-                    ->label('Nome')
-                    ->unique(ignoreRecord: true)
-                    ->required(),
-                TextInput::make('username')
-                    ->label('Login')
-                    ->unique(ignoreRecord: true)
-                    ->required(),
-                TextInput::make('email')
-                    ->label('E-mail')
-                    ->required()
-                    ->email(),
-                TextInput::make('password')
-                    ->label('Senha')
-                    ->password()
-                    ->revealable()
-                    ->required()
-                    ->rule('min:4')
-                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
-                    ->same('passwordConfirmation')
-                    ->validationAttribute('senha'),
-                TextInput::make('passwordConfirmation')
-                    ->label(__('filament-panels::pages/auth/register.form.password_confirmation.label'))
-                    ->password()
-                    ->required()
-                    ->revealable()
-                    ->dehydrated(false),
-                Toggle::make('is_admin')
-                    ->label('Administrador')
-                    ->inline(false)
-                    ->onIcon('heroicon-m-bolt')
-                    ->offIcon('heroicon-m-user'),
-                Forms\Components\Select::make('partners')
-                    ->columnSpanFull()
-                    ->label('Conveniados')
-                    ->multiple()
-                    ->relationship(
-                        name: 'partners',
-                        titleAttribute: 'name',
-                        modifyQueryUsing: fn (Builder $query) => $query->where('partner', 1),
+                Section::make('Dados do Usuário')
+                    ->description(
+                        fn (string $operation): string => $operation === 'create' || $operation === 'edit' ? 'Informe os campos solicitados' : ''
                     )
-                    ->getOptionLabelFromRecordUsing(fn (Person $record) => "{$record->registration} - {$record->name}")
-                    ->required(),
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                        TextInput::make('username')
+                            ->label('Login')
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('E-mail')
+                            ->required()
+                            ->email(),
+                        TextInput::make('password')
+                            ->label('Senha')
+                            ->password()
+                            ->revealable()
+                            ->required()
+                            ->rule('min:4')
+                            ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                            ->same('passwordConfirmation')
+                            ->validationAttribute('senha'),
+                        TextInput::make('passwordConfirmation')
+                            ->label(__('filament-panels::pages/auth/register.form.password_confirmation.label'))
+                            ->password()
+                            ->required()
+                            ->revealable()
+                            ->dehydrated(false),
+                        Toggle::make('is_admin')
+                            ->label('Administrador')
+                            ->inline(false)
+                            ->onIcon('heroicon-m-bolt')
+                            ->offIcon('heroicon-m-user'),
+                        Select::make('partners')
+                            ->columnSpanFull()
+                            ->label('Conveniados')
+                            ->multiple()
+                            ->relationship(
+                                name: 'partners',
+                                titleAttribute: 'name',
+                                modifyQueryUsing: fn (Builder $query) => $query->where('partner', 1),
+                            )
+                            ->getOptionLabelFromRecordUsing(fn (Person $record) => "{$record->registration} - {$record->name}")
+                            ->required(),
+                    ])
             ]);
     }
 
