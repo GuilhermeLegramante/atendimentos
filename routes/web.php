8a57c58c10a048a22480ed3eb779e1e673d26390
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-07-03 16:25:19 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,7 @@ Route::get('/salvar-sinais-tabela-nova', function () {
     }
 
     foreach ($signals as $key => $signal) {
-        if ($signal['id'] > 5284) {
+        if ($signal['id'] > 5617) {
             DB::connection('marcaesinal')
                 ->table('new_signals')
                 ->insertGetId([
