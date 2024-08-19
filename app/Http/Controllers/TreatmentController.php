785fb0336bb5791512570ccf8c18db459b5337de
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-08-19 11:46:01 -0300

    .

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -27,7 +27,7 @@ class TreatmentController extends Controller
     {
         $treatments = Treatment::withSum('providedServices', 'patient_value')->get();
 
-        $totalServices = ProvidedService::sum('value');
+        $totalServices = ProvidedService::selectRaw('SUM(value * quantity) as value')->value('value');
 
         $fileName = 'ATENDIMENTOS_REALIZADOS.pdf';
 
