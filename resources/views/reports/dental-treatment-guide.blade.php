c9e62a4d59eb213ad9db0343f2a7006cf69bd360
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-02-21 11:11:33 -0300

    .

--- resources/views/reports/dental-treatment-guide.blade.php
+++ resources/views/reports/dental-treatment-guide.blade.php
@@ -32,7 +32,7 @@
     <table class="striped fit" style="width: 100%; float: left; margin-right: 4%; border: 1px solid black;">
         <thead>
             <tr style="background-color:#f2f2f2; ">
-                <th colspan="5" style="background-color:#f2f2f2; color: black; border: 1px solid black;">
+                <th colspan="6" style="background-color:#f2f2f2; color: black; border: 1px solid black;">
                     Atendimentos Realizados
                 </th>
             </tr>
