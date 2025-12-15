45876c582d4c2fba094655e2cc04d77e9c3c18a6
GuilhermeLegramante <guilhermelegramante@gmail.com>
2025-12-15 10:57:08 -0300

    .

--- resources/views/reports/treatments.blade.php
+++ resources/views/reports/treatments.blade.php
@@ -71,7 +71,7 @@
                 </td>
             </tr>
         </table>
-        <table class="striped fit" style="width: 100%; float: left; margin-right: 4%; border: 1px solid black;">
+        <table class="striped fit" style="width: 100%; float: left; margin-right: 4%; border: 1px solid black; font-size: 14px;">
             <thead>
                 <tr style="background-color:#f2f2f2; ">
                     <th colspan="4" style="background-color:#f2f2f2; color: black; border: 1px solid black;">
